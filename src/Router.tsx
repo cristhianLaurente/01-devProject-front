e5f311e41ec062ceb6a09fe0caf160f1f5f222bf
chrisantiz <santzch@gmail.com>
2020-02-12 10:52:28 -0500

    Add react-bootstrap

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 const Home = React.lazy(() => import('./pages/home/Home'));
 const NotFound = React.lazy(() => import('./pages/404'));
@@ -7,7 +8,12 @@ const NotFound = React.lazy(() => import('./pages/404'));
 export default () => {
   return (
     <Router>
-      <React.Suspense fallback={<h1>show loader here...</h1>}>
+      <React.Suspense
+        fallback={
+          <Container>
+            <h1>show loader here...</h1>
+          </Container>
+        }>
         <Switch>
           {/* home page */}
           <Route exact path="/" component={Home}></Route>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Container, Modal } from 'react-bootstrap';
 
 export default () => {
-  return <h1>Home page works!</h1>;
+  const [modalStatus, setModalStatus] = useState(false);
+
+  // show/hide modal
+  const toggleModal = () => setModalStatus(!modalStatus);
+
+  return (
+    <Container>
+      <h1>Home page works!</h1>
+      <Button variant="primary" onClick={toggleModal}>bootstrap</Button>
+
+      <Modal show={modalStatus} onHide={toggleModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>Bootstrap modal works!</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={toggleModal}>Close</Button>
+          <Button variant="primary" onClick={toggleModal}>Save changes</Button>
+        </Modal.Footer>
+      </Modal>
+    </Container>
+  );
 };
